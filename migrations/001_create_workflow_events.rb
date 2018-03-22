7d81335e06cbb423f733dd81b3aa888e350c96e9
David Sevcik <david.sevcik@gmail.com>
2018-03-22 13:08:59 +0100

    Fix migration

--- migrations/001_create_workflow_events.rb
+++ migrations/001_create_workflow_events.rb
@@ -2,9 +2,9 @@ class CreateWorkflowEvents < ActiveRecord::Migration[5.0]
   def change
     create_table :workflow_events do |t|
       t.datetime :timestamp
-      t.datetime :issue_key
-      t.datetime :issue_title
-      t.datetime :issue_description
+      t.string :issue_key
+      t.string :issue_title
+      t.text :issue_description
       t.string :issue_labels, array: true
       t.jsonb :payload
     end
